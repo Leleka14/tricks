d264e8c17a639fbf7ed96d40dd8171ee053e720c
Leleka14 <43849847+Leleka14@users.noreply.github.com>
2021-07-27 17:02:51 +0300

    Update errors.js

--- errors.js
+++ errors.js
@@ -1,1 +1,10 @@
+1. > Task :app:packageReleaseBundle FAILED
 
+FAILURE: Build failed with an exception.
+
+* What went wrong:
+Execution failed for task ':app:packageReleaseBundle'.
+> A failure occurred while executing com.android.build.gradle.internal.tasks.Workers$ActionFacade
+   > At most one element <application> with namespace '' was expected, but 2 were found.
+   
+Solution - AndroidManifest.xml must have only 1 <application></application>
