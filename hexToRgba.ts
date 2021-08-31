3a56d5b42d80b2e6a3746faaeedde209d911eb04
Leleka14 <43849847+Leleka14@users.noreply.github.com>
2021-08-31 11:33:55 +0300

    Create hexToRgba.ts

---
+++ hexToRgba.ts
@@ -0,0 +1,10 @@
+export const hexToRgb = (hex: string, opacity: number) => {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (result) {
+    var r = parseInt(result[1], 16);
+    var g = parseInt(result[2], 16);
+    var b = parseInt(result[3], 16);
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+  }
+  return null;
+};
